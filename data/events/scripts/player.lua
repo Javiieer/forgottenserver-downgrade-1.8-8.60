510313f86907f7f6fcf16275512e7217cc68575c
Mateuzkl <mateus.roberto@educacao.mg.gov.br>
2026-04-13 15:47:50 -0300

    feat(config): add slow task warning toggle and move familiar look hook to default_onLook

--- data/events/scripts/player.lua
+++ data/events/scripts/player.lua
@@ -2,16 +2,6 @@ function Player:onLook(thing, position, distance)
 	local description = ""
 	if hasEvent.onLook then description = Event.onLook(self, thing, position, distance, description) end
 
-	if thing:isCreature() then
-		local master = thing:getMaster()
-		local summons = { "sorcerer familiar", "knight familiar", "druid familiar", "paladin familiar", "monk familiar" }
-		if master and table.contains(summons, thing:getName():lower()) then
-			local familiarSummonTime = master:getStorageValue(PlayerStorageKeys.familiarSummonTime) or 0
-			description = description .. " (Master: " .. master:getName() .. "). \z
-				It will disappear in " .. Game.getTimeInWords(familiarSummonTime - os.time())
-		end
-	end
-
 	if description ~= "" then self:sendTextMessage(MESSAGE_INFO_DESCR, description) end
 end
 
